58fc2c279767f62d84f95e342fface0f2c91ee97
Andlyn <andlyn666@126.com>
2023-10-26 16:12:31 +0800

    Update PlaygroundShareComsumer.sol
    Update PlaygroundShareComsumer.sol
    
    Update PlaygroundShareComsumer.sol

--- contracts/user/PlaygroundShareComsumer.sol
+++ contracts/user/PlaygroundShareComsumer.sol
@@ -16,9 +16,9 @@ contract PlaygroundShareComsumerContract is
     OwnableUpgradeable
 {
     // To be update
-    uint32 private constant DRAW_CALLBACK_FEE = 100000;
-    uint32 private constant ROLL_CALLBACK_FEE = 100000;
-    uint32 private constant FEE_OVERHEAD = 100000;
+    uint32 private constant DRAW_CALLBACK_GAS = 1000000;
+    uint32 private constant ROLL_CALLBACK_GAS = 1000000;
+    uint32 private constant FEE_OVERHEAD = 1000000;
     uint32 private constant GROUP_SIZE = 3;
 
     // common subId for trial
@@ -65,8 +65,16 @@ contract PlaygroundShareComsumerContract is
     error InsufficientFund(uint256 fundAmount, uint256 requiredAmount);
     error InvalidSubId();
 
-    constructor(address adapter) BasicRandcastConsumerBase(adapter) {}
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor(address adapter) BasicRandcastConsumerBase(adapter) {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init();
+    }
     // solhint-disable-next-line no-empty-blocks
+
     function _authorizeUpgrade(address) internal override onlyOwner {}
 
     function _fundSubId(PlayType playType, uint64 subId) internal returns (uint64) {
@@ -92,64 +100,58 @@ contract PlaygroundShareComsumerContract is
         return subId;
     }
 
-    function estimateFee(PlayType playType, uint64 subId) internal view returns (uint256 requestFee) {
-        IAdapter adapterInstance = IAdapter(adapter);
-
-        uint32 callbackGasLimit = playType == PlayType.Draw ? DRAW_CALLBACK_FEE : ROLL_CALLBACK_FEE;
-
-        // Get subscription details only if subId is not zero
+    struct Subscription {
         uint256 balance;
+        uint256 inflightCost;
         uint64 reqCount;
-        uint64 freeRequestCount = 1;
+        uint64 freeRequestCount;
         uint64 reqCountInCurrentPeriod;
         uint256 lastRequestTimestamp;
-        if (subId != 0) {
-            (,, balance,, reqCount, freeRequestCount,, reqCountInCurrentPeriod, lastRequestTimestamp) =
-                adapterInstance.getSubscription(subId);
-        }
-
-        uint32 tierFee = _calculateTierFee(reqCount, lastRequestTimestamp, reqCountInCurrentPeriod, freeRequestCount);
-
-        requestFee = adapterInstance.estimatePaymentAmountInETH(
-            callbackGasLimit, FEE_OVERHEAD, tierFee, tx.gasprice * 3, GROUP_SIZE
-        ) - balance;
     }
 
-    struct FeeConfig {
-        bool isFlatFeePromotionEnabledPermanently;
-        uint256 flatFeePromotionStartTimestamp;
-        uint256 flatFeePromotionEndTimestamp;
-        uint16 flatFeePromotionGlobalPercentage;
-    }
-
-    function _getFlatFeeConfig() internal view returns (FeeConfig memory feeConfig) {
-        IAdapter adapterInstance = IAdapter(adapter);
+    function getSubscription(uint64 subId) internal view returns (Subscription memory sub) {
         (
             ,
             ,
+            sub.balance,
+            sub.inflightCost,
+            sub.reqCount,
+            sub.freeRequestCount,
             ,
-            ,
-            ,
-            ,
-            ,
-            ,
-            ,
-            feeConfig.flatFeePromotionGlobalPercentage,
-            feeConfig.isFlatFeePromotionEnabledPermanently,
-            feeConfig.flatFeePromotionStartTimestamp,
-            feeConfig.flatFeePromotionEndTimestamp
-        ) = adapterInstance.getFlatFeeConfig();
+            sub.reqCountInCurrentPeriod,
+            sub.lastRequestTimestamp
+        ) = IAdapter(adapter).getSubscription(subId);
     }
 
-    function _calculateTierFee(
-        uint64 reqCount,
-        uint256 lastRequestTimestamp,
-        uint64 reqCountInCurrentPeriod,
-        uint64 freeRequestCount
-    ) internal view returns (uint32 tierFee) {
+    function estimateFee(PlayType playType, uint64 subId) public view returns (uint256 requestFee) {
+        uint32 callbackGasLimit = playType == PlayType.Draw ? DRAW_CALLBACK_GAS : ROLL_CALLBACK_GAS;
+        if (subId == 0) {
+            subId = userSubId[msg.sender];
+            if (subId == 0) {
+                return IAdapter(adapter).estimatePaymentAmountInETH(
+                    callbackGasLimit, FEE_OVERHEAD, 0, tx.gasprice * 3, GROUP_SIZE
+                );
+            }
+        }
+        // Get subscription details only if subId is not zero
+        Subscription memory sub = getSubscription(subId);
+        uint32 tierFee;
+        if (sub.freeRequestCount == 0) {
+            tierFee = _calculateTierFee(sub.reqCount, sub.lastRequestTimestamp, sub.reqCountInCurrentPeriod);
+        }
+        uint256 estimatedFee = IAdapter(adapter).estimatePaymentAmountInETH(
+            callbackGasLimit, FEE_OVERHEAD, tierFee, tx.gasprice * 3, GROUP_SIZE
+        );
+        return estimatedFee > (sub.balance - sub.inflightCost) ? estimatedFee - (sub.balance - sub.inflightCost) : 0;
+    }
+
+    function _calculateTierFee(uint64 reqCount, uint256 lastRequestTimestamp, uint64 reqCountInCurrentPeriod)
+        internal
+        view
+        returns (uint32 tierFee)
+    {
         // Use the new struct here.
         FeeConfig memory feeConfig = _getFlatFeeConfig();
-
         uint64 reqCountCalc;
         if (feeConfig.isFlatFeePromotionEnabledPermanently) {
             reqCountCalc = reqCount;
@@ -166,8 +168,38 @@ contract PlaygroundShareComsumerContract is
                 reqCountCalc = reqCountInCurrentPeriod + 1;
             }
         }
-        if (freeRequestCount == 0) {
-            tierFee = IAdapter(adapter).getFeeTier(reqCountCalc) * feeConfig.flatFeePromotionGlobalPercentage / 100;
+        return IAdapter(adapter).getFeeTier(reqCountCalc) * feeConfig.flatFeePromotionGlobalPercentage / 100;
+    }
+
+    struct FeeConfig {
+        uint16 flatFeePromotionGlobalPercentage;
+        bool isFlatFeePromotionEnabledPermanently;
+        uint256 flatFeePromotionStartTimestamp;
+        uint256 flatFeePromotionEndTimestamp;
+    }
+
+    function _getFlatFeeConfig() public view returns (FeeConfig memory feeConfig) {
+        {
+            (, bytes memory point) =
+            // solhint-disable-next-line avoid-low-level-calls
+             address(adapter).staticcall(abi.encodeWithSelector(IAdapter.getFlatFeeConfig.selector));
+            uint16 flatFeePromotionGlobalPercentage;
+            bool isFlatFeePromotionEnabledPermanently;
+            uint256 flatFeePromotionStartTimestamp;
+            uint256 flatFeePromotionEndTimestamp;
+            // solhint-disable-next-line no-inline-assembly
+            assembly {
+                flatFeePromotionGlobalPercentage := mload(add(point, 320))
+                isFlatFeePromotionEnabledPermanently := mload(add(point, 352))
+                flatFeePromotionStartTimestamp := mload(add(point, 384))
+                flatFeePromotionEndTimestamp := mload(add(point, 416))
+            }
+            feeConfig = FeeConfig(
+                flatFeePromotionGlobalPercentage,
+                isFlatFeePromotionEnabledPermanently,
+                flatFeePromotionStartTimestamp,
+                flatFeePromotionEndTimestamp
+            );
         }
     }
 
@@ -180,11 +212,9 @@ contract PlaygroundShareComsumerContract is
         bytes memory params;
         params = abi.encode(bunch);
         requestId = _rawRequestRandomness(
-            RequestType.RandomWords, params, subId, seed, requestConfirmations, ROLL_CALLBACK_FEE, tx.gasprice * 3
+            RequestType.RandomWords, params, subId, seed, requestConfirmations, ROLL_CALLBACK_GAS, tx.gasprice * 3
         );
-        emit requestRollEvent(
-            msg.sender, subId, requestId, msg.value, bunch, seed, requestConfirmations
-        );
+        emit requestRollEvent(msg.sender, subId, requestId, msg.value, bunch, seed, requestConfirmations);
         requestIdToRequestData[requestId] = RequestData(PlayType.Roll, abi.encode(bunch));
     }
 
@@ -198,17 +228,10 @@ contract PlaygroundShareComsumerContract is
         subId = _fundSubId(PlayType.Draw, subId);
         bytes memory params;
         requestId = _rawRequestRandomness(
-            RequestType.Randomness, params, subId, seed, requestConfirmations, DRAW_CALLBACK_FEE, tx.gasprice * 3
+            RequestType.Randomness, params, subId, seed, requestConfirmations, DRAW_CALLBACK_GAS, tx.gasprice * 3
         );
         emit requestDrawEvent(
-            msg.sender,
-            subId,
-            requestId,
-            msg.value,
-            totalNumber,
-            winnerNumber,
-            seed,
-            requestConfirmations
+            msg.sender, subId, requestId, msg.value, totalNumber, winnerNumber, seed, requestConfirmations
         );
         requestIdToRequestData[requestId] = RequestData(PlayType.Draw, abi.encode(totalNumber, winnerNumber));
     }
@@ -227,7 +250,6 @@ contract PlaygroundShareComsumerContract is
             emit RollResult(requestId, diceResults);
             delete requestIdToRequestData[requestId];
         }
-
     }
 
     /**
@@ -254,7 +276,6 @@ contract PlaygroundShareComsumerContract is
         }
         (,, uint256 balance,,,,,,) = IAdapter(adapter).getSubscription(subId);
         IAdapter(adapter).cancelSubscription(subId, msg.sender);
-        payable(msg.sender).transfer(balance);
         delete userSubId[msg.sender];
     }
 
